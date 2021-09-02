5621e09e392f19b5beec03e5c224dec734dab161
Hakkani Mim <mimhakkani4g@gmail.com>
2021-09-02 22:27:19 +0600

    final comments are set

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@
 const input = document.getElementById('input-id'); 
 const submit = document.getElementById('search-id'); 
 
+//main function is start here 
+
 const bookinput =() =>{
  const searchText = input.value
  input.value =''
- if(searchText==''){
+
+ //error handling for empty string
+ if(searchText===''){
      document.getElementById('error').style.display ='block'
  }
 else{
@@ -15,27 +19,24 @@ else{
 //book url api dynamic by name
 const bookurl =`https://openlibrary.org/search.json?q=${searchText}`
  
-//for books
+//for books fetch,response,data
+
  fetch(bookurl)
  .then(res=>res.json())
  .then(data =>BooksData(data.docs))
 
- 
-
 }
 
 
-
-
+//arrow function 2 works on to show single data value from data objects
 
 const BooksData =(books)=>{
-
 
  //show the number of search result from the online books store
     const numberR =document.getElementById('res-number');
     numberR.textContent=''
     numberR.innerHTML=`
-                <h3 class=" text-muted" style="">The Searching result is ${books.length}</h3>
+                <h3 class="text-muted search-number" style="">The Searching result is ${books.length}</h3>
     `
   
    //catch the main div element id
@@ -46,11 +47,11 @@ const BooksData =(books)=>{
 
        booklist.textContent =''
 
-    /* show signly data books from book  using for loops */
-    console.log(books);
+    /* show singley data books from book  using for each loop */
+   
     books.forEach(book =>{
-     
-      const div = document.createElement('div')
+
+     const div = document.createElement('div')
         div.classList.add('col-md-3')
         div.innerHTML =`
                 <div class="card mb-2">
@@ -67,6 +68,7 @@ const BooksData =(books)=>{
                     </div>
                   
                 </div>
+                
             
         `
         booklist.appendChild(div)
@@ -84,13 +86,13 @@ const bookmore =(moredetails)=>{
     const moreinfo = document.getElementById('more')
     moreinfo.textContent =''
     const p =document.createElement('p')
+    //add more than one class 
     p.classList.add('border')
     p.classList.add('p-4')
     p.classList.add('text-muted')
     p.innerText =moredetails
 
-    moreinfo.appendChild(p)
-   
-   
+    //append child
+    moreinfo.appendChild(p) 
 
 }
